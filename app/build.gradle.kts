f2130728eb6ebf43300b0ca34ef2343310935485
PT123123 <1031614639@qq.com>
2026-09-18 05:59:59 +0800

    chore(release): 1.0.3 桌面组件重构与已醒修复发版

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ted.shouhuan"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.0.3"
         vectorDrawables { useSupportLibrary = true }
     }
 
