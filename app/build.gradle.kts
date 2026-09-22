788796d2e2efeaa7897efbcc49409c09b21bf1b4
PT123123 <31439216+PT123123@users.noreply.github.com>
2026-09-22 22:05:50 +0800

    chore(release): 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ted.shouhuan"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.3"
+        versionCode = 7
+        versionName = "1.1.0"
         vectorDrawables { useSupportLibrary = true }
     }
 
