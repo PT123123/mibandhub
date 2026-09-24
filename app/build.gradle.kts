9b629aff49b1e0e57e1ce6c0441fac75211b5ce4
PT123123 <31439216+PT123123@users.noreply.github.com>
2026-09-25 04:42:08 +0800

    bump: 1.1.0 → 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ted.shouhuan"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.1.0"
+        versionCode = 8
+        versionName = "1.1.1"
         vectorDrawables { useSupportLibrary = true }
     }
 
